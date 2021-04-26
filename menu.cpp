453cc61a35357b309c434a3fcb361d30dfd6cb29
Daniel Rivera <38059166+riverada131@users.noreply.github.com>
2021-04-25 20:31:18 -0400

    add general menu struct to menu.hpp

--- menu.cpp
+++ menu.cpp
@@ -16,20 +16,21 @@ using std::cin;
 using std::endl;
 using std::string;
 
-/*Function name: press_enter_to_continue
+/*
+Function name: press_enter_to_continue
 Input: Enter - user presses enter key to continue
 Output: None
 Description: used to pause any process until the user presses enter.
 */
 void press_enter_to_continue() {
-	cout << string(7, ' '); //output of 7 spaces before printing the message.
+	cout << string(8, ' '); //output of 7 spaces before printing the message.
 	printf("Press enter to continue"); //prints the message for the user to press enter.
 	while(getchar() != '\n'); //while loop to wait until the user presses enter.
 }
 
-/*Function name: print_welcome
-Input:
-year_created - integer variable passed for copyright declaration. year inserted into the statement.
+/*
+Function name: print_welcome
+Input: year_created - integer variable passed for copyright declaration. year inserted into the statement.
 authors_name - pointer variable passed for the author's name and added to the splash screen.
 origination_date - pointer variable passed for the creation date of the program.
 program_name - pointer variable created for the programs name and displayed within the splash screen.
@@ -42,14 +43,27 @@ void splash::print_welcome(int year_created, string *authors_name, string *origi
 	int num_asterics = 43; //variable to set number of asterics on the top and bottom border
 
 	//Message outputs for the initial splash screen.
-	cout << string(num_asterics, '*') << endl;
-	cout << "* Author: " << *authors_name << string(19, ' ') << "*" << endl;
-	cout << "* Created on: " << *origination_date << string(19, ' ') << "*" << endl;
+	cout << string(num_asterics, '*') << "\n"
+	<< "* Author: " << *authors_name << string(19, ' ') << "*\n"
+	<< "* Created on: " << *origination_date << string(19, ' ') << "*" << endl;
 	printf("* Copyright, %i, All rights reserved    *\n", year_created);
-	cout << "* program name: " << *program_name << " *" << endl;
-	cout << string(num_asterics, '*') << endl;
+	cout << "* program name: " << *program_name << " *\n"
+	<< string(num_asterics, '*') << endl;
 
 	//function calls here
 	press_enter_to_continue();//function call to pause the screen until the user presses enter
 	system("clear");//system call to clear screen.
 };
+
+/* 
+Function Name:
+Input:
+Output:
+Description:
+*/
+int menu::primary_menu() {
+	int choice;
+	
+	return choice;
+};
+
--- menu.hpp
+++ menu.hpp
@@ -10,6 +10,14 @@ copyright 2021, Daniel Rivera, all rights reserved
 
 #include <string>
 
+typedef void(*menu_processing_function_pointer)(void);
+
+struct menu_options {
+	char choice;
+	const char *p_selection_text;
+	menu_processing_function_pointer p_processing_function;
+};
+
 using std::string;
 
 class splash {
@@ -23,7 +31,9 @@ class menu {
 private:
 
 public:
-
+	int primary_menu();
+	void process_selection_one();
+	void process_selection_two();
 };
 
 #endif /* MENU_HPP */
